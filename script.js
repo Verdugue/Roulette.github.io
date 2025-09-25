f5ca0057c705b1ccd559ef343508f7204a9cde07
Verdugue <144103486+Verdugue@users.noreply.github.com>
2025-09-25 17:34:10 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://roulettegithubio-production.up.railway.app/';
 const importBtn = document.getElementById('importBtn');
 const createBtn = document.getElementById('createBtn');
 const namesInput = document.getElementById('namesInput');
